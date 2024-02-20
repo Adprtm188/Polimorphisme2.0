286aff517605dc801a2986e922f8e7d469a974f7
Adprtm188 <156062888+Adprtm188@users.noreply.github.com>
2024-02-20 13:31:52 +0700

    Add files via upload

---
+++ abstrak/Shape.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abstrak;
+
+/**
+ *
+ * @author SMK TELKOM 44
+ */
+public abstract class Shape {
+       String color;
+    
+    void setColor(String color){
+        this.color = color;
+    }
+    
+    String getColor(){
+        return this.color;
+    }
+    
+    abstract float getArea();
+}
---
+++ abstrak/triangle.java
@@ -0,0 +1,16 @@
+package abstrak;
+public class triangle extends Shape{
+      private float base;
+    private float height;
+
+    public triangle(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    
+    @Override
+    float getArea() {
+        return 0.5f * base * height;
+    }
+}
